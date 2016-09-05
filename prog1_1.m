2826cb984f138cf2b6fa7882e6f8f16d2fc41a2c
Sam Friedman <samtx@outlook.com>
2016-09-05 11:37:18 -0500

    Update prog1_1.m

--- prog1_1.m
+++ prog1_1.m
@@ -7,36 +7,55 @@
 % -k*u''(x) + beta*u'(x) = f(x)
 % 0 < x < 1, u(0) = a, u(1) = b
 
-n = [25, 50, 100, 200];  % internal mesh points
-
 % approx u(x) ~ U(x)
 % Prob 1.1
-k = 1; f = 0; beta = [1, 8, 64]; a = 0; b = 1;
 
-i = 1; j = 1;
-h = (1-0)/(n(i)+1);
-f = ones(n,1)*f;
-u = zeros(n,1);
+prob = 1;
+part = 1;
+k = 1; f = 0; a = 0; b = 1;
 
-% create A matrix
-A = diag((-k - beta(j)*h/2)*ones(n(i)-1,1),-1) + ...
-    diag(2*k*ones(n(i),1)) + ...
-    diag((-k + beta(j)*h/2)*ones(n(i)-1,1),+1);
+for n = [25, 50, 100, 200]  % internal mesh points
+    
+    h = (b-a)/(n+1);  % set step size
+    x = [a+h:h:b-h]'; %#ok<NBRAK>
+    
+    data = table(x);
+    for beta = [1, 8, 64]  % beta values
+        
+        
+        f = ones(n,1)*f;  % init f vector
 
-% boundary conditions
-f(1) = f(1) - (-k - beta(j)*h/2) * a;      % left
-f(end) = f(end) - (-k + beta(j)*h/2) * b;  % right
+        
+        % create A matrix
+        A = diag((-k - beta*h/2)*ones(n-1,1),-1) + ...
+            diag(2*k*ones(n,1)) + ...
+            diag((-k + beta*h/2)*ones(n-1,1),+1);
+        
+        % boundary conditions
+        f(1) = f(1) - (-k - beta*h/2) * a;      % left
+        f(end) = f(end) - (-k + beta*h/2) * b;  % right
+        
+        % solve AU=f matrix equation
+        uapx = A\f;  % u approximate
+        
+        % exact solution
+        u_exact = @(X) (1 - exp(beta*X))/(1 - exp(beta));
+        uext = u_exact(x); 
+        
+        % compute error
+        e = uext - uapx;
+        
+        plot(x,uext,'-k',x,uapx,'--r','Linewidth',2)
+        legend('Exact Solution','Approx Solution');
+        xlabel('x');ylabel('y')
+        
+        dlmwrite
+        e_1p1b1n25 = e;
+        uext_1p1b1n25 = u;
+        uapx_1p1b1n25 = U;
+        break
+    end
+    break
+end
 
-U = A\f;  % solve AU=f matrix equation
-
-% exact solution
-u_exact = @(x) (1 - exp(beta(j)*x))/(1 - exp(beta(j))); 
-u = u_exact(a+h:h:b-h);
-u = u';
-
-% compute error
-e = u - U;
-x = a+h:h:b-h;
-plot(x,u,'-k',x,U,'--r','Linewidth',2)
-legend('Exact Solution','Approx Solution');
-xlabel('x');ylabel('y')
+% export data to .dat file
