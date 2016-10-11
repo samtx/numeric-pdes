4818abb093def9cfbb1a9a008051670149fa4b25
Sam Friedman <samtx@outlook.com>
2016-10-11 12:54:54 -0500

    Add files via upload

--- hw3/hw3.m
+++ hw3/hw3.m
@@ -59,16 +59,10 @@ for p = 1:length(poly);
             phi0{1} = @(s) -0.5*s + 0.5;
             phi0{2} = @(s) 0.5*s + 0.5;
             phi1{1} = @(s) -0.5;  % derivative
-            phi1{2} = @(s) 0.5; 
+            phi1{2} = @(s) 0.5;
             phi2{1} = @(s) 0;
             phi2{2} = @(s) 0;
         case 2  % quadratic
-%             phi{1} = @(s) (2*s - 1)*(s - 1);
-%             phi{2} = @(s) 4*s*(s - 1);
-%             phi{3} = @(s) s*(2*s - 1);
-%             phiprm{1} = @(s) 4*s - 3;
-%             phiprm{2} = @(s) -8*s + 4;
-%             phiprm{3} = @(s) 4*s - 1;
             phi0{1} = @(s) 0.5*s^2 - 0.5*s;
             phi0{2} = @(s) -s^2 + 1;
             phi0{3} = @(s) 0.5*s^2 + 0.5*s;
@@ -105,151 +99,151 @@ for p = 1:length(poly);
             
             f0 = @(s) phi0{i}(s)*phi0{j}(s);  % function to integrate
             % integration of f0 over -1,1 using 4 point quadrature
-            A0(i,j) = wi(1)*f0(xi(1)) + wi(2)*f0(xi(2)) + wi(3)*f0(xi(3)) + wi(4)*f0(xi(4));  
+            A0(i,j) = wi(1)*f0(xi(1)) + wi(2)*f0(xi(2)) + wi(3)*f0(xi(3)) + wi(4)*f0(xi(4));
             
             f1 = @(s) phi1{i}(s)*phi1{j}(s);  % derivative function to integrate
             % integration of f1 over -1,1 using 4 point quadrature
-            A1(i,j) =  wi(1)*f1(xi(1)) + wi(2)*f1(xi(2)) + wi(3)*f1(xi(3)) + wi(4)*f1(xi(4)); 
+            A1(i,j) =  wi(1)*f1(xi(1)) + wi(2)*f1(xi(2)) + wi(3)*f1(xi(3)) + wi(4)*f1(xi(4));
             
             f2 = @(s) phi2{i}(s)*phi2{j}(s);  % second derivative function to integrate
             % integration of f2 over -1,1 using 4 point quadrature
-            A2(i,j) =  wi(1)*f2(xi(1)) + wi(2)*f2(xi(2)) + wi(3)*f2(xi(3)) + wi(4)*f2(xi(4)); 
-        
+            A2(i,j) =  wi(1)*f2(xi(1)) + wi(2)*f2(xi(2)) + wi(3)*f2(xi(3)) + wi(4)*f2(xi(4));
+            
         end
     end
     
-   % copy upper triangular part to lower triangular part for symmetry
-   A0 = A0 + triu(A0,1)';  
-   A1 = A1 + triu(A1,1)';
-   
-   allA{p} = A0;
-   allAprm{p} = A1;
-   
-   
-   % init global matrices
-   glbA0 = zeros(N*P+1);
-   glbA1 = zeros(N*P+1);
-   glbA2 = zeros(N*P+1);
-
-t0 = 0; tf = 1;
-k = 1; beta = 0; 
-a = 0; b = 0;  % b.c. values
-
-% constants
-q = 200;    % lb/in2
-D = 8.8e7;  % lb*in
-L = 50;     % in
-
-
-
-sList = [100, 1000, 10000];  % S values,  [lb/in]
-
-
-L2 = zeros(length(nn),length(sList));
-Linf = zeros(length(nn),length(sList));
-
-nrms = table(nn);
-nrms.Properties.VariableNames = {'n'};
-
-for i = 1:length(nn);
+    % copy upper triangular part to lower triangular part for symmetry
+    A0 = A0 + triu(A0,1)';
+    A1 = A1 + triu(A1,1)';
     
-    n = nn(i);
+    allA{p} = A0;
+    allAprm{p} = A1;
     
-    h = (tf-t0)/(n+1);  % set step size
-    t = [t0+h:h:tf-h]'; %#ok<NBRAK>
-    soln = table(t);  % init soln data table with FD points
-      
-    for j = 1:length(sList)
+    
+    % init global matrices
+    glbA0 = zeros(N*P+1);
+    glbA1 = zeros(N*P+1);
+    glbA2 = zeros(N*P+1);
+    
+    t0 = 0; tf = 1;
+    k = 1; beta = 0;
+    a = 0; b = 0;  % b.c. values
+    
+    % constants
+    q = 200;    % lb/in2
+    D = 8.8e7;  % lb*in
+    L = 50;     % in
+    
+    
+    
+    sList = [100, 1000, 10000];  % S values,  [lb/in]
+    
+    
+    L2 = zeros(length(nn),length(sList));
+    Linf = zeros(length(nn),length(sList));
+    
+    nrms = table(nn);
+    nrms.Properties.VariableNames = {'n'};
+    
+    for i = 1:length(nn);
         
-        S = sList(j);
+        n = nn(i);
         
-        gamma = S*(L^2)/D;
+        h = (tf-t0)/(n+1);  % set step size
+        t = [t0+h:h:tf-h]'; %#ok<NBRAK>
+        soln = table(t);  % init soln data table with FD points
         
-        Z = q*(L^4)/(2*D);
+        for j = 1:length(sList)
             
-        % build f(t) vector
-        ft = Z*t.*(1-t);
+            S = sList(j);
+            
+            gamma = S*(L^2)/D;
+            
+            Z = q*(L^4)/(2*D);
+            
+            % build f(t) vector
+            ft = Z*t.*(1-t);
+            
+            f = ft*(h^2);  % init f vector
+            
+            %         % create coefficient vector
+            %         c(1) = -k;
+            %         c(2) = 2*k + gamma*h^2 ;
+            %         c(3) = -k;
+            %
+            %         % create A matrix
+            %         A = diag(c(1)*ones(n-1,1),-1) + ...
+            %             diag(c(2)*ones(n,1)) + ...
+            %             diag(c(3)*ones(n-1,1),+1);
+            
+            % boundary conditions
+            f(1) = f(1) - c(1)*a;      % left
+            f(end) = f(end) - c(3)*b;  % right
+            
+            % solve AU=f matrix equation
+            uapx = A0\f;  % u approximate
+            
+            % exact solution
+            u_exact = @(t) Z/gamma*(-t.^2 + t-2/gamma + ...
+                2/(gamma * sinh(sqrt(gamma))) * ...
+                (sinh(sqrt(gamma)*t) + sinh(sqrt(gamma)*(1-t))));
+            uext = u_exact(t);
+            
+            % compute error
+            e = uext - uapx;
+            
+            % save norms data to matrices
+            L2(i,j) = norm(e,2);
+            Linf(i,j) = norm(e,Inf);
+            
+            % take abs value and add eps to error for graphing purposes
+            e = abs(e) + eps;
+            
+            % save solution data to table
+            solnsfix = ['S',num2str(S)];
+            solnvars = {['uext', solnsfix],['uapx', solnsfix],['e', solnsfix]};
+            tmp = table(uext,uapx,e);
+            tmp.Properties.VariableNames = solnvars;
+            soln = [soln, tmp]; %#ok<AGROW> % concatenate tmp table with solutions table
+            
+        end
         
-        f = ft*(h^2);  % init f vector
-        
-%         % create coefficient vector
-%         c(1) = -k;
-%         c(2) = 2*k + gamma*h^2 ;
-%         c(3) = -k;       
-%         
-%         % create A matrix
-%         A = diag(c(1)*ones(n-1,1),-1) + ...
-%             diag(c(2)*ones(n,1)) + ...
-%             diag(c(3)*ones(n-1,1),+1);
-        
-        % boundary conditions
-        f(1) = f(1) - c(1)*a;      % left
-        f(end) = f(end) - c(3)*b;  % right
-        
-        % solve AU=f matrix equation
-        uapx = A0\f;  % u approximate
-        
-        % exact solution
-        u_exact = @(t) Z/gamma*(-t.^2 + t-2/gamma + ...
-            2/(gamma * sinh(sqrt(gamma))) * ...
-            (sinh(sqrt(gamma)*t) + sinh(sqrt(gamma)*(1-t))));
-        uext = u_exact(t);
-        
-        % compute error
-        e = uext - uapx;
-        
-        % save norms data to matrices
-        L2(i,j) = norm(e,2);
-        Linf(i,j) = norm(e,Inf);
-        
-        % take abs value and add eps to error for graphing purposes
-        e = abs(e) + eps;      
-        
-        % save solution data to table
-        solnsfix = ['S',num2str(S)];
-        solnvars = {['uext', solnsfix],['uapx', solnsfix],['e', solnsfix]};
-        tmp = table(uext,uapx,e);
-        tmp.Properties.VariableNames = solnvars;
-        soln = [soln, tmp]; %#ok<AGROW> % concatenate tmp table with solutions table
+        % write solutions table to data file for each n value
+        writetable(soln,[prbsfx,'n',num2str(n),'soln','.dat'],'Delimiter','\t');
         
     end
     
-    % write solutions table to data file for each n value
-    writetable(soln,[prbsfx,'n',num2str(n),'soln','.dat'],'Delimiter','\t');
+    % export data to .dat file
     
-end
-
-% export data to .dat file
-
-nrms = zeros(size(L2,1),size(L2,2)*2+1);
-nrmsvars = {'n'};
-nrms(:,1) = nn;
-
-for j = 1:size(L2,2)
+    nrms = zeros(size(L2,1),size(L2,2)*2+1);
+    nrmsvars = {'n'};
+    nrms(:,1) = nn;
     
-    nrms(:,j*2) = L2(:,j);
-    nrms(:,j*2+1) = Linf(:,j);
+    for j = 1:size(L2,2)
+        
+        nrms(:,j*2) = L2(:,j);
+        nrms(:,j*2+1) = Linf(:,j);
+        
+        S = sList(j);
+        nrmssfix = ['S',num2str(S)];
+        nrmsvars = [nrmsvars, {['L2', nrmssfix], ['Linf', nrmssfix]}]; %#ok<AGROW>
+        
+    end
     
-    S = sList(j);
-    nrmssfix = ['S',num2str(S)];
-    nrmsvars = [nrmsvars, {['L2', nrmssfix], ['Linf', nrmssfix]}]; %#ok<AGROW>
+    nrmtbl = array2table(nrms);
+    nrmtbl.Properties.VariableNames = nrmsvars;
+    writetable(nrmtbl,[prbsfx,'nrms','.dat'],'Delimiter','\t');
     
-end
-
-nrmtbl = array2table(nrms);
-nrmtbl.Properties.VariableNames = nrmsvars;
-writetable(nrmtbl,[prbsfx,'nrms','.dat'],'Delimiter','\t');
-
-   
-%    soln = table(x);  % init soln data table with FD points
     
-%     % save solution data to table
-%     solnsfix = ['ply',num2str(ply)];
-%     solnvars = {['uext', solnsfix],['uapx', solnsfix],['e', solnsfix]};
-%     tmp = table(uext,uapx,e);
-%     tmp.Properties.VariableNames = solnvars;
-%     soln = [soln, tmp]; %#ok<AGROW> % concatenate tmp table with solutions table
-%     
+    %    soln = table(x);  % init soln data table with FD points
+    
+    %     % save solution data to table
+    %     solnsfix = ['ply',num2str(ply)];
+    %     solnvars = {['uext', solnsfix],['uapx', solnsfix],['e', solnsfix]};
+    %     tmp = table(uext,uapx,e);
+    %     tmp.Properties.VariableNames = solnvars;
+    %     soln = [soln, tmp]; %#ok<AGROW> % concatenate tmp table with solutions table
+    %
 end
 %
 %     % write solutions table to data file for each n value
@@ -281,7 +275,6 @@ end
 % s = sym(L);
 % v = vpa(s,5); % assign numerical precision
 % latex(v)
-
 
 
 
