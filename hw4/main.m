57b3fad4c43a1db89bf75f73c710516acbc09519
Sam Friedman <samtx@outlook.com>
2016-11-09 21:32:52 -0600

    same

--- hw4/main.m
+++ hw4/main.m
@@ -8,12 +8,12 @@
 nnode = 3;  % nodes per element
 q = 5;  % constant
 
-prob = 2;
-
-fname = 'mesh/p2n10';
+prob = 3;
+g = 1;
+fname = 'mesh/p3n40';
 
 [n, verts, elems, bounds] =  get_mesh(fname);
-draw_mesh(verts,elems);
+% draw_mesh(verts,elems);
 
 if prob == 1
     
@@ -23,9 +23,18 @@ if prob == 1
     
 elseif prob == 2
     
-    [uapx, time, X, Y] = hw4pb2(n, verts, elems, bounds);
+    [uapx, time, X, Y] = hw4pb2(n, verts, elems, bounds, g);
     
     [plot1] = plot_soln2(elems, X, Y, uapx);
+    
+elseif prob == 3
+    
+    f = @(x,y) x.*y;
+        
+    [uapx, time, X, Y] = fem2d_elliptic(n, verts, elems, bounds, f, g);
+    
+    [plot1] = plot_soln2(elems, X, Y, uapx);
+    
     
 end
 
